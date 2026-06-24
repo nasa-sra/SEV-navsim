b32d6787db74d3985919a0ce3279485e65654158
Nuhan Hasan <nmh.07000@gmail.com>
2026-06-24 08:58:34 -0500

    added autonomous stanley control!

--- sim_screen.py
+++ sim_screen.py
@@ -1,6 +1,5 @@
 import pygame
 import math
-
 import button
 import waypoints
 import vehicle
@@ -23,6 +22,10 @@ class SimScreen:
         self.deadband = 0.1
         self.xValue = 0
         self.yValue = 0
+        self.cte = 0
+
+        self.autoMode = False
+        self.targetSpeed = 150
 
         if self.useJoystick:
             self.joystick = pygame.joystick.Joystick(0)
@@ -63,6 +66,28 @@ class SimScreen:
             waypoints.removeWaypoint
         )
 
+        self.start_button = button.Button(
+            colors.GREEN,
+            (0, 150, 0),
+            900,
+            160,
+            75,
+            50,
+            "START",
+            self.start_autonomous
+        )
+
+        self.stop_button = button.Button(
+            colors.RED,
+            colors.DARK_RED,
+            975,
+            160,
+            75,
+            50,
+            "STOP",
+            self.stop_autonomous
+        )
+
         self.vJoystick = virtual_joystick.VirtualJoystick(
             900,
             500
@@ -82,6 +107,12 @@ class SimScreen:
             700,
             150
         )
+        
+        self.headingDisplay = display_text.Text(
+            "",
+            700,
+            200
+        )
 
         self.cteDisplay = display_text.Text(
             "",
@@ -93,6 +124,12 @@ class SimScreen:
 
         self.reset_sim()
 
+    def start_autonomous(self):
+        self.autoMode = True
+
+    def stop_autonomous(self):
+        self.autoMode = False
+
     def reset_sim(self):
 
         self.SEV.heading = 0
@@ -103,6 +140,8 @@ class SimScreen:
 
         if hasattr(self.SEV, "speed"):
             self.SEV.speed = 0
+
+        self.autoMode = False
 
         waypoints.resetWaypoints()
 
@@ -124,43 +163,87 @@ class SimScreen:
         self.addWaypoint_button.handle_event(event)
         self.removeWaypoint_button.handle_event(event)
 
+        self.start_button.handle_event(event)
+        self.stop_button.handle_event(event)
+
         waypoints.handle_event(event)
 
-    def update(self, dt):
+    def follow_waypoints(self, dt):
 
-        keys = pygame.key.get_pressed()
+        wps = waypoints.getWaypoints()
+        
+        last_wp_coords = wps[-1].getCenter()
+        
+        if(self.closestPoint == last_wp_coords and self.SEV.pos.distance_to(last_wp_coords) < 150):
+            self.autoMode = False
+            return
 
-        throttle = 0
-        steering = 0
+        if len(wps) < 2:
+            return
 
-        if keys[pygame.K_w]:
-            throttle = 250
+        wpIndex = 0
 
-        elif keys[pygame.K_s]:
-            throttle = -250
+        self.closestPoint = pygame.Vector2(
+            10000,
+            10000
+        )
 
-        elif self.useJoystick:
-            y = 0 if abs(self.yValue) < self.deadband else self.yValue
-            throttle = -y * 250
+        for i in range(len(wps) - 1):
 
-        if keys[pygame.K_a]:
-            steering = -80
+            curPoint = (
+                self.stanleyController
+                .closest_point_on_segment(
+                    self.SEV.pos,
+                    wps[i].getCenter(),
+                    wps[i + 1].getCenter()
+                )
+            )
 
-        elif keys[pygame.K_d]:
-            steering = 80
+            if (
+                self.closestPoint.distance_to(self.SEV.pos)
+                >
+                curPoint.distance_to(self.SEV.pos)
+            ):
+                self.closestPoint = curPoint
+                wpIndex = i
 
-        elif self.useJoystick:
-            x = 0 if abs(self.xValue) < self.deadband else self.xValue
-            steering = x * 80
+        steering = self.stanleyController.steering(
+            self.SEV.pos,
+            self.SEV.heading,
+            self.SEV.speed,
+            wps[wpIndex],
+            wps[wpIndex + 1]
+        )
+
+        steering = max(
+            -100,
+            min(100, steering)
+        )
+
+        speedError = (
+            self.targetSpeed
+            - self.SEV.speed
+        )
+
+        throttle = speedError * 3
+
+        throttle = max(
+            -250,
+            min(250, throttle)
+        )
 
         self.SEV.speed += throttle * dt
 
         drag = 2.0
-        self.SEV.speed -= self.SEV.speed * drag * dt
+        self.SEV.speed -= (
+            self.SEV.speed * drag * dt
+        )
 
-        self.SEV.heading += steering * dt
+        self.SEV.heading += steering * 0.4 * dt
 
-        heading_rad = math.radians(self.SEV.heading)
+        heading_rad = math.radians(
+            self.SEV.heading
+        )
 
         self.SEV.pos.x += (
             math.cos(heading_rad)
@@ -173,6 +256,74 @@ class SimScreen:
             * self.SEV.speed
             * dt
         )
+
+    def update(self, dt):
+
+        if self.autoMode:
+            self.follow_waypoints(dt)
+        else:
+
+            keys = pygame.key.get_pressed()
+
+            throttle = 0
+            steering = 0
+
+            if keys[pygame.K_w]:
+                throttle = 250
+
+            elif keys[pygame.K_s]:
+                throttle = -250
+
+            elif self.useJoystick:
+                y = (
+                    0
+                    if abs(self.yValue)
+                    < self.deadband
+                    else self.yValue
+                )
+
+                throttle = -y * 250
+
+            if keys[pygame.K_a]:
+                steering = -100
+
+            elif keys[pygame.K_d]:
+                steering = 100
+
+            elif self.useJoystick:
+                x = (
+                    0
+                    if abs(self.xValue)
+                    < self.deadband
+                    else self.xValue
+                )
+
+                steering = x * 100
+
+            self.SEV.speed += throttle * dt
+
+            drag = 2.0
+            self.SEV.speed -= (
+                self.SEV.speed * drag * dt
+            )
+
+            self.SEV.heading += steering * dt
+
+            heading_rad = math.radians(
+                self.SEV.heading
+            )
+
+            self.SEV.pos.x += (
+                math.cos(heading_rad)
+                * self.SEV.speed
+                * dt
+            )
+
+            self.SEV.pos.y += (
+                math.sin(heading_rad)
+                * self.SEV.speed
+                * dt
+            )
 
         wps = waypoints.getWaypoints()
 
@@ -215,7 +366,7 @@ class SimScreen:
                 2
             )
 
-            cte = round(
+            self.cte = round(
                 self.stanleyController.compute_cte(
                     self.SEV.pos,
                     wps[wpIndex].getCenter(),
@@ -229,7 +380,11 @@ class SimScreen:
             )
 
             self.cteDisplay.updateText(
-                f"current CTE: {cte}"
+                f"current CTE: {self.cte}"
+            )
+            
+            self.headingDisplay.updateText(
+                f"current heading: {round(self.SEV.heading, 2)}"
             )
 
     def draw(self):
@@ -240,6 +395,9 @@ class SimScreen:
         self.addWaypoint_button.draw(self.screen)
         self.removeWaypoint_button.draw(self.screen)
 
+        self.start_button.draw(self.screen)
+        self.stop_button.draw(self.screen)
+
         waypoints.draw(self.screen)
 
         self.vJoystick.draw(self.screen)
@@ -247,6 +405,7 @@ class SimScreen:
 
         self.steeringDisplay.display(self.screen)
         self.cteDisplay.display(self.screen)
+        self.headingDisplay.display(self.screen)
 
         vehicle_surface = pygame.Surface(
             (
